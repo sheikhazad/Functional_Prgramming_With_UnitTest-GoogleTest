b2e00af452246654c2dc92b6ae4906117ff2a5e9
sheikhazad <sheikhazad2@yahoo.com>
2019-06-23 06:49:25 +0100

    Functional Programming Demo with GoogleTest

---
+++ FunctionalProgramming.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+/***************************************************************************
+1. A simple demo of functional programming.
+2. T = Vector or Deque
+3. TFun - Function is passed as argument by the caller, it is called again from this function.
+***************************************************************************/
+template<typename TFunc, typename T>
+void filterData(TFunc fun, T& iContainer, T& oContainer )
+{
+    for(auto& anElement : iContainer)
+    {
+        //Calling fun - Function which was passed as argument by the caller
+        if(fun(anElement)) {
+           oContainer.emplace_back(anElement);
+        }
+    }
+}
+
+//1. Test if the functional program filtered those elements which are less than equal to 50
+TEST(FuntionalProgramTest, testLessThanEqual_50)
+{
+    vector<int> iVec{10,20,30,40,50,60,70,80,90,100};
+    vector<int> oVec;
+
+    //Reserve oVec to avoid re-allocation
+    oVec.reserve(iVec.size());
+
+    //Lambda function is passed as parameter to filterData()
+    //auto type for lambda function parameter is C++14 feature
+    filterData([](auto& i) ->bool { return i<=50; }, iVec, oVec );
+
+    //Release unused memory from oVec
+    oVec.shrink_to_fit();
+
+    cout<<"\nOutput result from Functional Program, Less than equal to 50 : " << endl << endl;
+    for(auto& anElement : oVec)
+    {
+       cout<< anElement << " ";
+       EXPECT_LE(anElement, 50);
+    }
+}
+
+//2. Test if the functional program filtered those elements which are between 30 and 80
+TEST(FuntionalProgramTest, testBetween_30_And_80)
+{
+    deque<int> iDeque{10,20,30,40,50,60,70,80,90,100};
+    deque<int> oDeque;
+
+    //Lambda function is passed as parameter to filterData()
+    //auto type for lambda function parameter is C++14 feature
+    filterData([](auto& i)->bool { return i>=30 && i<=80; }, iDeque, oDeque );
+
+    cout<<"\nOutput result from Functional Program, between 30 and 80: " << endl << endl;
+    for(auto& anElement : oDeque)
+    {
+       cout<< anElement << " ";
+       EXPECT_LE(anElement, 80);
+       EXPECT_GE(anElement, 30);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
